769555128e7d7811d1722218865319c0ca369a46
heyimcarlos <carlos.cruzta@gmail.com>
2026-08-12 16:57:52 -0400

    chore: remove clutter

--- infra/modules/command-buffer/variables.tf
+++
@@ -1,6 +0,0 @@
-variable "enabled" { type = bool }
-variable "project_id" { type = string }
-variable "region" { type = string }
-variable "name_prefix" { type = string }
-variable "message_retention_duration" { type = string }
-variable "labels" { type = map(string) }
